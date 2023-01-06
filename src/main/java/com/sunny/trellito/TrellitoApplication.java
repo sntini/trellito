57c2dca5bb256e736be34964b675d8124a0a98ab
sunny <sunny.ntini+github@gmail.com>
2023-01-06 18:33:02 -0500

    added project files

---
+++ src/main/java/com/sunny/trellito/TrellitoApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.trellito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrellitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrellitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunny/trellito/TrellitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.trellito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrellitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
